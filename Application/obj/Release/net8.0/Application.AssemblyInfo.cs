5eb3be08f494fbdf467c2ba7593c55b4123fa27e
Shafiqul Hayat <opashafiq@gmail.com>
2024-06-26 09:29:00 +0600

    Before Gitignore Edit

--- Application/obj/Release/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Release/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c065838d125359d0809d83965751b815c668e0b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd9cd0a9878c14ecd327a43841e1209cee38c5fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
