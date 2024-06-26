623641f110083ef75cf60a4c28443e0981227c59
Shafiqul Hayat <opashafiq@gmail.com>
2024-06-26 11:29:04 +0600

    Implemented Branch Code

--- Application/Operations/BranchMaster/CommandHandlers/CreateBranchMasterHandler.cs
+++ Application/Operations/BranchMaster/CommandHandlers/CreateBranchMasterHandler.cs
@@ -27,10 +27,57 @@ namespace Application.Operations.BranchMaster.CommandHandlers
 
         async Task<Domain.Entities.BranchMaster> IRequestHandler<CreateBranchMaster, Domain.Entities.BranchMaster>.Handle(CreateBranchMaster request, CancellationToken cancellationToken)
         {
+            // Get The task
+            var branchMasterCollectionTask = _branchMasterRepository.GetAll();
+            // Await the task to get the collection
+            var branchMasterCollection = await branchMasterCollectionTask;
+            // Calculate the new BranchCode
+            var newBranchCodeSuffix = GenerateNewBranchCode(branchMasterCollection);
 
             var branchMaster = _mapper.Map<Domain.Entities.BranchMaster>(request);
 
+            string countryPrefix = "";
+            string cityPrefix = "";
+
+            // Take 2 character from Country
+            if (branchMaster.BranchCountry.Length < 2)
+            {
+                countryPrefix = branchMaster.BranchCountry.ToUpper();
+            }
+            else
+            {
+                countryPrefix = branchMaster.BranchCountry.Substring(0, 2).ToUpper();
+            }
+
+            // Take 2 character from City
+            if (branchMaster.BranchCity.Length < 3)
+            {
+                cityPrefix = branchMaster.BranchCity.ToUpper();
+            }
+            else
+            {
+                cityPrefix = branchMaster.BranchCity.Substring(0, 3).ToUpper();
+            }
+
+            branchMaster.BranchCode = countryPrefix + cityPrefix + newBranchCodeSuffix;
+
             return await _branchMasterRepository.AddBranchMaster(branchMaster);
+        }
+
+        public static string GenerateNewBranchCode(ICollection<Domain.Entities.BranchMaster> collection)
+        {
+            // Fetch the maximum BranchCode, take rightmost 2 characters, convert to bigint, and add 1
+            var maxBranchCodeSuffix = collection
+                .Where(item => !string.IsNullOrEmpty(item.BranchCode) && item.BranchCode.Length >= 2)
+                .Select(item => item.BranchCode.Substring(item.BranchCode.Length - 2))
+                .Select(vendorCodeSuffix => long.TryParse(vendorCodeSuffix, out var result) ? result : 0)
+                .DefaultIfEmpty(0)
+                .Max() + 1;
+
+            var newBranchCodeSuffix = maxBranchCodeSuffix.ToString().PadLeft(2, '0');
+            //var newBranchCode = "SUP" + newBranchCodeSuffix;
+
+            return newBranchCodeSuffix;
         }
     }
 }
--- Domain/obj/Release/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Release/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd9cd0a9878c14ecd327a43841e1209cee38c5fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58d5ed335952387c4ac3987f5374092acd94144e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
